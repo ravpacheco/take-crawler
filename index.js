c41e75348c9a9e4d80e018957462fb188754d34e
Rafael Pacheco <ravpacheco@gmail.com>
2018-04-15 20:57:19 -0300

    Add endpoints to get current open jobs and blog posts by category

--- index.js
+++ index.js
@@ -1,42 +1,85 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    //res.send('Hello World!');
+app.get('/jobs', function (req, res) {
 
     var rp = require('request-promise');
     var cheerio = require('cheerio'); // Basically jQuery for node.js
 
-    var $ = cheerio.load('<div class="row jobs"> <div class="col-xs-12 col-sm-6 job"> <div class="row"> <div class="col-xs-12 col-sm-5 vcenter"> <figure> <img src="/wp-content/uploads/2016/10/vaga_po.png" alt="Vaga"> </figure> </div> <div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-6 vcenter"> <p>Gestor de Projetos (GP)/ Product Owner (PO)</p> <p>S&#xE3;o Paulo/SP</p> <a href="http://www.take.net/trabalhe-na-take/gp-po/">mais informa&#xE7;&#xF5;es</a> </div> </div> </div> <div class="col-xs-12 col-sm-6 job"> <div class="row"> <div class="col-xs-12 col-sm-5 vcenter"> <figure> <img src="/wp-content/uploads/2016/10/vaga_po.png" alt="Vaga"> </figure> </div> <div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-6 vcenter"> <p>Analista de Pr&#xE9;-Vendas (SDR)</p> <p>Belo Horizonte/MG</p> <a href="http://www.take.net/trabalhe-na-take/analista-de-pre-vendas/">mais informa&#xE7;&#xF5;es</a> </div> </div> </div></div>');
+    let currentJobs = [];
 
-    var fruits = [];
-    $('.col-xs-12.col-sm-6.job').each(function(i, elem) {
-        fruits[i] = $(this).html();
-    });
+    var options = {
+        uri: 'http://www.take.net/trabalhe-na-take/',
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
 
-    console.log(fruits);
+    const jobDivsClassesPattern = ".col-xs-offset-1.col-xs-10.col-sm-offset-0.col-sm-6.vcenter";
 
-    // var options = {
-    //     uri: 'http://www.take.net/trabalhe-na-take/',
-    //     transform: function (body) {
-    //         return cheerio.load(body);
-    //     }
-    // };
+    rp(options)
+        .then(function ($) {
+            // Process html like you would with jQuery...
+            $(jobDivsClassesPattern).each(function (i, elem) {
+                let data = $(this).find('p');
+                let jobName = $(data[0]).text();
+                let jobLocation = $(data[1]).text();
+                let jobInfoUrl = $(this).find('a').attr('href');
+                console.log(`${i}> ${jobName}, ${jobLocation}, ${jobInfoUrl}`);
 
-    // rp(options)
-    //     .then(function ($) {
-    //         // Process html like you would with jQuery...
-    //         var x = $('.row .jobs').text();
-    //         console.log(x);
+                currentJobs.push({
+                    name: jobName,
+                    location: jobLocation,
+                    infoUrl: jobInfoUrl
+                });
+            });
 
-    //         //.toArray()
+            res.send(currentJobs);
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
+});
 
-    //     })
-    //     .catch(function (err) {
-    //         // Crawling failed or Cheerio choked...
-    //     });
+app.get('/posts/:category', function (req, res) {
 
+    var rp = require('request-promise');
+    var cheerio = require('cheerio'); // Basically jQuery for node.js
 
+    let category = req.params.category;
+    let posts = [];
+
+    var options = {
+        uri: `https://chatbotsbrasil.take.net/tag/${category}`,
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
+
+    const postsClassesPattern = ".blog-item-wrap";
+
+    rp(options)
+        .then(function ($) {
+            // Process html like you would with jQuery...
+            $(postsClassesPattern).each(function (i, elem) {
+                let metaData = $(this).find('a');
+                let postUrl = $(this).find('a').attr('href');
+                let postTitle = $(this).find('a').attr('title');
+                let postImageUrl = $($(this).find('a img')[0]).attr('src');
+                console.log(`${i}> ${postUrl}, ${postTitle}, ${postImageUrl}`);
+
+                posts.push({
+                    url: postUrl,
+                    title: postTitle,
+                    imageUrl: postImageUrl
+                });
+            });
+
+            res.send(posts);
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 });
 
 app.listen(3000, function () {
