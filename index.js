6fc99443d3196cf148997e91a192dfd7d7dd1fac
Rafael Pacheco <ravpacheco@gmail.com>
2018-04-23 23:12:54 -0300

    Add dynamic port for application

--- index.js
+++ index.js
@@ -92,6 +92,6 @@ app.get('/posts/:category*?', function (req, res) {
         });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
